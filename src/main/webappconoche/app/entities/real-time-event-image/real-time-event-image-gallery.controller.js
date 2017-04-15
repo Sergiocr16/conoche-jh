93e0b2d3a8e3cab7302f02a83112508f307de1d0
DESKTOP-MR9Q9JP\melvin <guntanis666@hotmail.com>
2017-04-15 08:48:51 -0600

    refactorize el controlador de la galleria.

--- src/main/webappconoche/app/entities/real-time-event-image/real-time-event-image-gallery.controller.js
+++ src/main/webappconoche/app/entities/real-time-event-image/real-time-event-image-gallery.controller.js
@@ -138,8 +138,7 @@
                 scope: $scope,
                 size: 'lg'
             });
-            index = $index;
-            vm.image = vm.realTimeEventImages[index];
+            setImage($index);
         }
 
         function close() {
@@ -151,231 +150,29 @@
 
         function refreshIndex() {
             var images = vm.realTimeEventImages;
-            if (vm.realTimeEventImages.length === 0) {
+            if (images.length === 0) {
                 return close();
             }
-            var i = _.findIndex(vm.realTimeEventImages, function(img) {
+            var i = _.findIndex(images, function(img) {
                 return vm.image.id == img.id;
             });
-            index = i >= 0 ? i : index % vm.realTimeEventImages.length;
-            vm.image = images[index];
+            setImage(i >= 0 ? i : index % images.length);
+
         }
 
         function next() {
             var images = vm.realTimeEventImages;
-            index = (index + 1) % images.length
-            vm.image = images[index];
+            setImage((index + 1) % images.length)
         }
 
         function prev() {
             var images = vm.realTimeEventImages;
-            index = (images.length + (index - 1)) % images.length
-            vm.image = images[index];
+            setImage((images.length + (index - 1)) % images.length);
+        }
+
+        function setImage(i) {
+            index = i;
+            vm.image = vm.realTimeEventImages[index];
         }
     }
-/*
-    function RealTimeEventImageGalleryController($scope, RealTimeEventImage, ParseLinks, AlertService, idEvent, WSRealTimeEventImages, $timeout) {
-        const THUMBNAIL_PADDING = 10
-        const ITEMS_PER_PAGE    = 10;
-        const SORT = 'creationTime,desc';
-
-        var vm = this;
-        var deleteQueue = [];
-        var createQueue = [];
-        var loadingPage = false;
-
-        vm.width = 330;
-        vm.border = 5;
-        vm.infiniteScrollDisable = true;
-        vm.loadPage = loadPage;
-        vm.page = 0;
-        vm.links = {
-            last: 0
-        };
-        vm.reset = reset;
-        vm.computeDimentions = computeDimentions;
-        vm.toggleFullScreen = toggleFullScreen;
-        vm.deleteImage = deleteImage;
-
-        init();
-        WSRealTimeEventImages.receiveNewImages(idEvent)
-            .then(null, null, addNewImage);
-
-        WSRealTimeEventImages.receiveDeleteImages(idEvent)
-            .then(null, null, onImageDeleted);
-
-        function init() {
-            realTimeEventImages(onSuccess, onError);
-            function onSuccess(data) {
-                vm.realTimeEventImages = data;
-            }
-        }
-
-        function toggleFullScreen(image) {
-            image.fullScreen = !image.fullScreen;
-        }
-
-        function computeDimentions(aspectRatio) {
-            var borderOffset = 2 * vm.border + THUMBNAIL_PADDING;
-            return {
-                'width' : vm.width + borderOffset,
-                'height' :  (vm.width * aspectRatio) + borderOffset
-            }
-        }
-
-        function realTimeEventImages(callback, onError) {
-            loadingPage = true;
-            vm.infiniteScrollDisable = true;
-            RealTimeEventImage.eventRealTimeImages({
-                idEvent: idEvent,
-                page: vm.page,
-                size: ITEMS_PER_PAGE,
-                sort: SORT
-            }, onSuccess, onError);
-
-            function onSuccess(data, headers) {
-                substractMetadataFromHeaders(headers);
-                callback(data);
-                clearDeleteQueue();
-                loadingPage = false;
-                timeoutDisable();
-            }
-        }
-
-        function clearDeleteQueue() {
-            console.log(deleteQueue.length, deleteQueue);
-            _.forEach(deleteQueue, function(callback) {
-                callback();
-            });
-            deleteQueue = [];
-        }
-
-        function substractMetadataFromHeaders(headers) {
-            vm.links = ParseLinks.parse(headers('link'));
-            vm.totalItems = headers('X-Total-Count');
-        }
-
-        function loadAll () {
-            realTimeEventImages(onSuccess, onError);
-            function onSuccess(data) {
-                vm.realTimeEventImages = vm.realTimeEventImages.concat(data);
-                $scope.$emit('masonry.reloaded');
-            }
-        }
-
-        function onError(error) {
-            AlertService.error(error.data.message);
-            vm.infiniteScrollDisable = false;
-            loadingPage = false;
-        }
-
-        function contains(image) {
-            var item = _.find( vm.realTimeEventImages, function(img) {
-                return img.id === image.id;
-            });
-            return item !== undefined;
-        }
-
-        function timeoutDisable() {
-            $timeout(function() { vm.infiniteScrollDisable = false; } , 400);
-        }
-
-        function addNewImage(image) {
-            if(!vm.realTimeEventImages
-                || contains(image)) {
-                return;
-            }
-            if(vm.realTimeEventImages.length % ITEMS_PER_PAGE === 0) {
-                vm.realTimeEventImages.pop();
-            }
-            vm.links.last = Math.floor(vm.totalItems++ / ITEMS_PER_PAGE);
-            vm.realTimeEventImages.unshift(image);
-        }
-
-        function deleteImage(image) {
-            WSRealTimeEventImages.deleteImage(image);
-        }
-
-        function onImageDeleted(image) {
-            if(loadingPage) {
-                deleteQueue.push(function () { addDeletedImage(image); });
-            }
-            else {
-                addDeletedImage(image);
-            }
-        }
-
-        function addDeletedImage(image) {
-            var index = findIndex(vm.realTimeEventImages, image.id);
-            if(index < 0) { return; }
-
-            vm.infiniteScrollDisable = true;
-            vm.realTimeEventImages.splice(index, 1)
-            if(vm.page < vm.links.last) {
-                addNextImage(vm.realTimeEventImages);
-            }
-            vm.links.last = Math.floor(vm.totalItems-- / ITEMS_PER_PAGE);
-            vm.page = Math.min(vm.page, vm.links.last);
-        }
-
-        function findIndex(items, id) {
-           return _.findIndex(items, function(current) {
-                return current.id === id;
-            });
-            return item !== undefined;
-        }
-
-        function timeoutDisable() {
-            $timeout(function() {vm.infiniteScrollDisable = false;} , 400);
-        }
-
-        function addNewImage(image) {
-            if(!vm.realTimeEventImages
-                || contains(image)) {
-                return;
-            }
-            if(vm.realTimeEventImages.length % ITEMS_PER_PAGE === 0) {
-                vm.realTimeEventImages.pop();
-            }
-            vm.links.last = Math.floor(vm.totalItems++ / ITEMS_PER_PAGE);
-            vm.realTimeEventImages.unshift(image);
-        }
-
-        function reset () {
-            vm.page = 0;
-            init();
-        }
-
-        function loadPage(page) {
-            vm.page = page;
-            loadAll();
-        }
-
-        function addNextImage() {
-            RealTimeEventImage.eventRealTimeImages({
-                idEvent: idEvent,
-                page:  vm.realTimeEventImages.length,
-                size: 1,
-                sort: SORT
-            }, onSuccessNextImage, onError);
-            function onSuccessNextImage(data) {
-               if(vm.realTimeEventImages.length % ITEMS_PER_PAGE !== 0) {
-                   vm.realTimeEventImages.push(data[0]);
-               }
-               vm.infiniteScrollDisable = false;
-            }
-        }
-
-        function reset () {
-            vm.page = 0;
-            init();
-        }
-
-        function loadPage(page) {
-            vm.page = page;
-            loadAll();
-        }
-    }
-
-    */
 })();
